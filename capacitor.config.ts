d153fdb844ab1e001e0480118c91d85c60f8b865
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-09-02 10:07:05 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bnow.app',
+  appName: 'mycart',
+  webDir: 'dist'
+};
+
+export default config;
